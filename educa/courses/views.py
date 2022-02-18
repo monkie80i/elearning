6e4f5a9adb0b645ca5de208ea47cbe904728d7ba
Shahzan Sadick <sam.shahzan@gmail.com>
2022-02-18 17:19:41 +0530

    templates

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from re import template
+from .models import Course
+from django.views.generic.list import ListView
 
 # Create your views here.
+class ManageCourseListView(ListView):
+    model = Course
+    template = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
