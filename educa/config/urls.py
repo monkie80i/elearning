12fa60edfb63d475a32e2b891e5ad52ec651a651
Shahzan Sadick <sam.shahzan@gmail.com>
2022-02-20 15:54:15 +0530

    restart

--- educa/config/urls.py
+++ educa/config/urls.py
@@ -1,4 +1,4 @@
-"""educa URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('accounts/login',auth_views.LoginView.as_view(),name='login'),
-    path('accounts/logout',auth_views.LogoutView.as_view(),name='logout'),
     path('admin/', admin.site.urls),
 ]
