da852695d9dc258b90153bed4ca29a5176d692a1
Shahzan Sadick <sam.shahzan@gmail.com>
2022-02-20 21:45:47 +0530

    custom admin

--- educa/adminManager/admin.py
+++ educa/adminManager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User,Profile
 
 # Register your models here.
+class EducaAdminSite(admin.AdminSite):
+	site_header = 'Ecom Admin Interface'
+	site_title = 'Ecom Administration'
+	index_title = 'Ecom Administration'
+	site_url = None
+
+admin_site = EducaAdminSite(name="EducaAdmin")
+
+admin_site.register(User)
+admin_site.register(Profile)
