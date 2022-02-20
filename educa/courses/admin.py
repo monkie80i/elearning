86242862e44cf8ef05cf759d48bd040b72efadc2
Shahzan Sadick <sam.shahzan@gmail.com>
2022-02-20 22:20:23 +0530

    added courses models to admin site

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -1,3 +1,27 @@
+from pyexpat import model
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-
+from .models import Subject,Course,Module
+from adminManager.admin import admin_site
 # Register your models here.
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    prepopulated_fields = {'slug':('title',)}
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+class CousrseAdmin(admin.ModelAdmin):
+    list_display = ['title','subject','created']
+    list_filter = ['created','subject']
+    search_fields = ['title','overview']
+    prepopulated_fields = {'slug':('title',)}
+    inlines = [ModuleInline]
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['id','title','created']
+
+admin_site.register(Subject,SubjectAdmin)
+admin_site.register(Course,CousrseAdmin)
+admin_site.register(Module,ModuleAdmin)
--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,44 @@
+from distutils.archive_util import make_zipfile
 from django.db import models
+from adminManager.models import User
 
 # Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=100,null=True,blank=True)
+    slug = models.SlugField(max_length=200,unique=True,null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+    
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+    user = models.ForeignKey(User,related_name = 'courses',on_delete=models.DO_NOTHING,null=True,blank=True)
+    subject = models.OneToOneField(Subject,on_delete=models.DO_NOTHING,null=True,blank=True)
+    title = models.CharField(max_length=100,null=True,blank=True)
+    slug = models.SlugField(max_length=200,unique=True,null=True,blank=True)
+    overview = models.TextField(null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.title
+
+class Module(models.Model):
+    course = models.ForeignKey(Course,related_name='modules',on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=100,null=True,blank=True)
+    description = models.TextField(null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created']
+    
+    def __str__(self):
+        return self.title
