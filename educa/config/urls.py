5d03e07fc4115a647366cf25ce4fee68da31de97
Shahzan Sadick <sam.shahzan@gmail.com>
2022-03-11 22:00:10 +0530

    static root

--- educa/config/urls.py
+++ educa/config/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('webpack-test/',TemplateView.as_view(template_name='test.html')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
