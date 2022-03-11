8310323fbfb76750797fe95670efa32e3f7feea9
Shahzan Sadick <sam.shahzan@gmail.com>
2022-03-11 20:20:24 +0530

    Revert "setting"
    This reverts commit f28d657076715b30bc4884b65d771d28e1958e68.

--- educa/config/settings.py
+++ educa/config/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #local
-    #'utils',
+    'utils',
     'adminManager',
-    #'courses',
+    'courses',
     #third party
     'embed_video',
 ]
