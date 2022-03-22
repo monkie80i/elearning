ffe5d82ec7ec33d38692b8cf56cf613e381014fd
Shahzan Sadick <sam.shahzan@gmail.com>
2022-03-22 17:05:01 +0530

    student permission

--- educa/courses/api/permissions.py
+++ educa/courses/api/permissions.py
@@ -7,4 +7,9 @@ class IsTeacher(permissions.BasePermission):
             return request.user.is_teacher
         return False
 
-    
+class IsStudent(permissions.BasePermission):
+
+    def has_permission(self, request,view):
+        if request.user.is_authenticated:
+            return request.user.is_student
+        return False
